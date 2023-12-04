4fa41bb3a85061131788a9eeb35ca016bd793f71
Sena Aydin <senaaydin484@gmail.com>
2023-12-04 19:29:12 +0100

    lab done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -28,28 +28,46 @@ router.post("/movies/create", (req, res) => {
 });
 
 router.get("/movies/:movieId", (req, res) => {
-  const {movieId} = req.params;
+  const { movieId } = req.params;
   Movie.findById(movieId)
-   .populate("cast")
+    .populate("cast")
     .then((foundMovie) => {
       res.render("movies/movie-details", { movie: foundMovie });
     })
     .catch((err) => console.log(err));
 });
 
-router.get("/movies/:movieId/delete", (req, res) => {
-    const {movieId} = req.params;
-    Movie.findByIdAndRemove(movieId)
-      .then(() => {
-        res.redirect("/movies");
-      })
-      .catch((err) => console.log(err));
-  });
+router.post("/movies/:movieId/delete", (req, res) => {
+  const { movieId } = req.params;
+  Movie.findByIdAndRemove(movieId)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => console.log(err));
+});
 
-  router.get("/movies/:movieId/edit", (req, res) => {
-    const{ movieId } = req.params;
-    Movie.findById(movieId)
-    .then(res.render("movies/edit-movie"))
-  })
-  
+router.get("/movies/:movieId/edit", (req, res) => {
+  const { movieId } = req.params;
+  Movie.findById(movieId)
+    .then((foundMovie) => {
+      Celebrity.find({})
+        .then((celebrities) => {
+          res.render("movies/edit-movie", { movie: foundMovie, celebrities });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/movies/:movieId/edit", (req, res) => {
+  const { movieId } = req.params;
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.findByIdAndUpdate(movieId, { title, genre, plot, cast })
+    .then(() => {
+      res.redirect(`/movies/${movieId}`);
+    })
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
